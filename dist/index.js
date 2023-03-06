c4134cee137a1485d21e22b6d172f6d5542cf3da
IevhenBuival <kdotbull@gmail.com>
2023-03-06 17:48:58 +0200

    created

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+app.get("/",(req,res)=>{
+    res.end('<h1>"home page"</h1>');
+})
+
+app.get("/about",(req,res)=>{
+    res.end('<h1>About</h1>');
+})
+
+app.listen(PORT,()=>{
+    console.log("started");
+});
